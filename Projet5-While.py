e07cc161c6f8b49afb248bc66faec089c4269962
Cimon <michaud.honda92@outlook.com>
2024-05-08 18:23:59 -0400

    V3

---
+++ Projet5-While.py
@@ -0,0 +1,4 @@
+x = 1
+while x < 10:
+    print('x est plus petit que 10, il est presentement egal a', x)
+    x += 1
--- Projet6-WhileElseElif.py
+++ Projet6-WhileElseElif.py
@@ -2,7 +2,9 @@ x = 1
 while x < 10:
     print('x a pour valeur', x, 'et est donc plus petit que 10')
     x = x + 1
-elif x = 11:
+if x == 11:
     print('x a 11 pour valeur')
+elif x == 12:
+    print('x a 12 pour valeur')
 else:
     print('x est maintenant plus grand que 10')
