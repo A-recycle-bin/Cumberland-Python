d35c0054d36b04f15e9419feefcbe14f15b4df99
Cimon <michaud.honda92@outlook.com>
2024-05-28 18:05:25 -0400

    V1

--- Projet47-TP-GestionListeObjet.py
+++ Projet47-TP-GestionListeObjet.py
@@ -38,20 +38,112 @@ class ListePersonnes:
             print(f"Aucune personne trouvée avec un âge entre {min_age} et {max_age}.")
 
 
-liste_personnes = ListePersonnes()
-liste_personnes.ajouter_personne("Alice", 25)
-liste_personnes.ajouter_personne("Bob", 30)
-liste_personnes.ajouter_personne("Cimon", 20)
-liste_personnes.ajouter_personne("Donald", 22)
-liste_personnes.ajouter_personne("Erman", 9)
-liste_personnes.ajouter_personne("Poulet", 27)
+class FileAttente:
+    def __init__(self):
+        self.file = []
+
+    def ajouter_personne_en_attente(self, nom, age):
+        nouvelle_personne = Personne(nom, age)
+        # le false va definir que la personne est reguliere
+        self.file.append((nouvelle_personne, False))
+
+    def ajouter_personne_importante(self, nom, age):
+        nouvelle_personne = Personne(nom, age)
+        # le true va definir que la personne est importante
+        self.file.insert(0,(nouvelle_personne, True))
+
+    # def supprimer_personne_en_attente(self, nom, age):
+    #     self.file.pop(nom)
+    #     self.file.pop(age)
+
+
+
+
+vLP = ListePersonnes()
+vLP.ajouter_personne("Alice", 25)
+vLP.ajouter_personne("Bob", 30)
+vLP.ajouter_personne("Cimon", 20)
+vLP.ajouter_personne("Donald", 22)
+vLP.ajouter_personne("Erman", 9)
+vLP.ajouter_personne("Poulet", 27)
 
 print("Liste de personnes:")
-liste_personnes.afficher_personnes()
+vLP.afficher_personnes()
 
 print("\nRecherche de personne:")
-liste_personnes.rechercher_personne("Bob")
+vLP.rechercher_personne("Bob")
 print("\nFiltrage par âge entre 20 et 25 ans:")
-liste_personnes.filtrer_personnes_par_age(20, 25)
+vLP.filtrer_personnes_par_age(20, 25)
 print("\nFiltrage par âge entre 10 et 33 ans:")
-liste_personnes.filtrer_personnes_par_age(10, 33)
+vLP.filtrer_personnes_par_age(10, 33)
+
+vFA = FileAttente()
+# vFA.ajouter_personne_en_attente('Tit-gris', 66)
+# vFA.ajouter_personne_importante('Tit-noir', 2)
+# vFA.ajouter_personne_en_attente('Tit-blanc', 33)
+# vFA.supprimer_personne_en_attente('Tit-gris', 66)
+
+print("\n\n\n\n")
+
+class SalleCinema:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.reserved_places = []
+        self.special_places = []
+
+    def reserver_place(self, nom, place):
+        if self.nombre_places_disponibles() > 0:
+            self.reserved_places.append((nom, place))
+            print(f"Une place a été réservée {nom} à la place {place} avec succès.")
+        else:
+            print("La place n,est pas disponible.")
+
+    def afficher_places_reservees(self):
+        print("Les places réservées sont les suivantes :")
+        for nom, place in self.reserved_places:
+            print(f"{nom} - Place {place}")
+
+    def nombre_places_disponibles(self):
+        return self.capacity - len(self.reserved_places)
+
+    def filtrer_reservations_par_personne(self, nom):
+        print(f"Reservations pour {nom}:")
+        for n, place in self.reserved_places:
+            if n == nom:
+                print(f"{n} - Place {place}")
+
+    def annuler_reservation(self, nom):
+        self.reserved_places = [(n, place) for n, place in self.reserved_places if n != nom]
+        print(f"Les réservations pour {nom} ont été annulées.")
+
+    def reserver_place_speciale(self, nom):
+        self.special_places.append(nom)
+        print(f"une place spéciale est réservée pour {nom}.")
+
+    def afficher_places_reservees_speciales(self):
+        print("Les places réservées spéciales sont les suivantes :")
+        for nom in self.special_places:
+            print(f"{nom} a réservé une place spéciales")
+
+# Example Usage
+cine = SalleCinema(50)
+print("Places restantes :", cine.nombre_places_disponibles())
+cine.reserver_place('Jacques', 2)
+print("Places restantes :", cine.nombre_places_disponibles())
+print('Nombre de places réservées : ', cine.afficher_places_reservees())
+cine.reserver_place("Alice", 10)
+cine.reserver_place("Bob", 20)
+cine.reserver_place("Cimon", 30)
+cine.reserver_place("Cimon", 31)
+
+cine.afficher_places_reservees()
+print("Places restantes :", cine.nombre_places_disponibles())
+cine.filtrer_reservations_par_personne("Alice")
+cine.annuler_reservation("Alice")
+cine.reserver_place_speciale("Julie-Rose")
+cine.afficher_places_reservees()
+cine.afficher_places_reservees_speciales()
+
+
+
+
